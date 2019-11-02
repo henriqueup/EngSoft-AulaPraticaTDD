e90e1f7e334df2cbcf5f9358583c1f5a28217869
Henrique Urban Pessoa <titepessoa@gmail.com>
2019-11-02 13:39:58 -0300

    COMMIT #5

--- Money.java
+++ Money.java
@@ -1,4 +1,4 @@
-abstract class Money {
+class Money {
 	protected int amount;
 	protected String currency;
 
@@ -8,18 +8,20 @@ abstract class Money {
 
 	public boolean equals(Object object){
 		Money money = (Money) object;
-		return amount == money.amount && getClass().equals(money.getClass());
+		return amount == money.amount && currency().equals(money.currency());
 	}
 
 	static Money dollar(int amount){
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 
 	static Money franc(int amount){
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 
-	abstract Money times(int multiplier);
+	Money times(int multiplier){
+		return new Money(amount * multiplier, currency);
+	}
 
 	Money(int amount, String currency){
 		this.amount = amount;
